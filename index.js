12468768962480b6c5db65e38df6b06cef1e0cb4
mahade <mahade.adib45@gmail.com>
2024-06-04 09:39:07 +0600

    pets data added

--- index.js
+++ index.js
@@ -25,13 +25,22 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        //await client.connect();
+
+        const PetsCollection = client.db("PawsDB").collection("pets");
+
+        //pets related api
+        app.get('/pets', async (req, res) => {
+            const result = await PetsCollection.find().toArray();
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        //await client.db("admin").command({ ping: 1 });
+        //console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        //await client.close();
     }
 }
 run().catch(console.dir);
