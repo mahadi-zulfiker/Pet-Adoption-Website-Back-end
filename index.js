c5ef3d9165794e9823000df6b352e72244a17f00
mahade <mahade.adib45@gmail.com>
2024-06-04 15:53:31 +0600

    pet details page added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -34,6 +34,11 @@ async function run() {
             const result = await PetsCollection.find().toArray();
             res.send(result);
         })
+
+        app.get("/pets/:id", async (req, res) => {
+            const result = await PetsCollection.findOne({ _id: new ObjectId(req.params.id), });
+            res.send(result)
+          })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
