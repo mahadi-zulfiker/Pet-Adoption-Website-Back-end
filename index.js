73a7d605c6dec9619ea39fce6ee6196fa4316fcd
mahade <mahade.adib45@gmail.com>
2024-06-09 12:39:45 +0600

    users admin verified and jwt oken genrated

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -41,6 +42,30 @@ async function run() {
         const UsersCollection = client.db("PawsDB").collection("users");
         const DonationCollection = client.db("PawsDB").collection("donations");
 
+        // jwt related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '9h'
+            });
+            res.send({ token });
+        })
+
+        //middlewares
+        const verifyToken = (req, res, next) => {
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+            const token = req.headers.authorization.split(' ')[1]
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(403).send({ message: 'forbidden access' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+
         //pets related api
         app.get('/pets', async (req, res) => {
             const result = await PetsCollection.find().toArray();
@@ -65,12 +90,12 @@ async function run() {
 
         // users related api
 
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyToken, async (req, res) => {
             const result = await UsersCollection.find().toArray();
             res.send(result);
         });
 
-        app.get('/users/admin/:email', async (req, res) => {
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
             const email = req.params.email;
             if (email !== req.decoded.email) {
                 return res.status(403).send({ message: 'forbidden access' })
