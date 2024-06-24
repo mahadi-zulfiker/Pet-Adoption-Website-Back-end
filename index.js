938ccb79f274ac876458d182477c65f3efff28ea
mahade <mahade.adib45@gmail.com>
2024-06-24 15:21:22 +0600

    updated

--- index.js
+++ index.js
@@ -398,6 +398,7 @@ async function run() {
             const result = await UsersCollection.deleteOne(query);
             res.send(result);
         })
+        
 
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
